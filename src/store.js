bdbe810a5a596ab4a81b338e7ffc3d89606a72d3
Jonathan Moore <jmjonemoore@gmail.com>
2019-06-14 14:47:58 -0500

    Added some testing

--- src/store.js
+++ src/store.js
@@ -4,6 +4,13 @@ const bookmarks = [{
   url: 'http://www.facebook.com',
   desc: 'Facebook home page',
   rating: 5
+},
+{
+  id: '2',
+  title: 'MLB.com',
+  url: 'http://www.mlb.com',
+  desc: 'MLB home page',
+  rating: 5
 }]
 
 module.exports = { bookmarks }
