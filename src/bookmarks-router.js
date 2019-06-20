1596ca9b4210905cdbd659cff6dcaca64481808d
Jonathan Moore <jmjonemoore@gmail.com>
2019-06-20 09:35:53 -0500

    Refactored GET / endpoints

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -1,33 +1,40 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = express.json()
 const { bookmarks } = require('./store')
 const uuid = require('uuid/v4')
 const logger = require('./logger')
 const { isWebUri } = require('valid-url')
+const BookmarksService = require('./bookmarks-service')
 
 const bookmarksRouter = express.Router()
 
 bookmarksRouter
   .route('/bookmarks')
-  .get((req, res) => {
-    res
-      .json(bookmarks)
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    BookmarksService.getAllBookmarks(knexInstance)
+      .then(bookmarks => {
+        res
+          .json(bookmarks)
+      })
+      .catch(next)
   })
   .post(bodyParser, (req, res) => {
     const { title, url, desc, rating } = req.body
 
-    if(!Number.isInteger(rating) || rating < 0 || rating > 5) {
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
       logger.error(`Invalid rating '${rating}' supplied`)
       return res
-      .status(400)
-      .send(`Invalid data`)
+        .status(400)
+        .send(`Invalid data`)
     }
 
-    if(!isWebUri(url)) {
+    if (!isWebUri(url)) {
       logger.error(`Invalid url '${url}' supplied`)
       return res
-      .status(400)
-      .send(`Invalid data`)
+        .status(400)
+        .send(`Invalid data`)
     }
 
     if (!title) {
@@ -44,18 +51,18 @@ bookmarksRouter
         .send('Invalid data')
     }
 
-    if(!desc) {
+    if (!desc) {
       logger.error(`Description is required.`)
       return res
-      .status(400)
-      .send('Invalid data')
+        .status(400)
+        .send('Invalid data')
     }
 
-    if(!rating){
+    if (!rating) {
       logger.error(`Rating is required.`)
       return res
-      .status(400)
-      .send('Invalid data')
+        .status(400)
+        .send('Invalid data')
     }
 
     const id = uuid()
@@ -81,7 +88,21 @@ bookmarksRouter
 
 bookmarksRouter
   .route('/bookmarks/:id')
-  .get((req, res) => {
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    BookmarksService.getById(knexInstance, req.params.id)
+      .then(bookmark => {
+        if (!bookmark) {
+          logger.error(`Bookmark with id ${req.params.id}`)
+          return res
+            .status(404)
+            .send('Bookmark Not Found')
+        }
+        res.json(bookmark)
+      })
+      .catch(next)
+    })
+  .delete((req, res) => {
     const { id } = req.params
     const bookmark = bookmarks.find(b => b.id === id)
 
@@ -92,28 +113,15 @@ bookmarksRouter
         .send('Bookmark Not Found')
     }
 
-    res.json(bookmark)
-  })
-  .delete((req, res) => {
-    const { id } = req.params
-    const bookmark = bookmarks.find(b => b.id === id)
-
-    if(!bookmark){
-      logger.error(`Bookmark with id ${id} not found.`)
-      return res
-      .status(404)
-      .send('Bookmark Not Found')
-    }
-
     const index = bookmarks.indexOf(bookmark)
-  
+
     bookmarks.splice(index, 1)
 
     logger.info(`Bookmark with id ${id} deleted`)
 
     res
-    .status(204)
-    .end()
+      .status(204)
+      .end()
   })
 
 module.exports = bookmarksRouter
---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,18 @@
+function makeBookmarksArray (){
+  return [{
+    id: 1,
+    title: 'Facebook.com',
+    uri: 'http://www.facebook.com',
+    descript: 'Facebook home page',
+    rating: '5'
+  },
+  {
+    id: 2,
+    title: 'MLB.com',
+    uri: 'http://www.mlb.com',
+    descript: 'MLB home page',
+    rating: '5'
+  }]
+}
+
+module.exports = { makeBookmarksArray }
